8e59630f17ade38d31a47c07a32f614264fed7e1
adithyag17 <97718839+adithyag17@users.noreply.github.com>
2025-03-23 23:20:35 +0530

    PORT

--- Backend/main.py
+++ Backend/main.py
@@ -40,4 +40,4 @@ def read_root():
 # Entry point
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=process.env.PORT)
