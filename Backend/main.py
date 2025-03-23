8296adb044419d65b90bd7bb85ba5f486558d16f
adithyag17 <97718839+adithyag17@users.noreply.github.com>
2025-03-23 23:56:49 +0530

    middleware allow

--- Backend/main.py
+++ Backend/main.py
@@ -19,7 +19,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly allow OPTIONS
     allow_headers=["Content-Type", "Authorization", "Accept"],
